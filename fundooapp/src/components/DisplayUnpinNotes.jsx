83e963848124a8a25794087848d7795e5a96c9d0
varad <varad2011>
2020-05-06 23:54:55 +0530

    create unpin note display class file

---
+++ fundooapp/src/components/DisplayUnpinNotes.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PinDropIcon from '@material-ui/icons/PinDrop';
+import { Card } from '@material-ui/core';
+import NoteIconOpration from '../components/NoteIconOpration'
+class DisplayUnpinNotes extends Component {
+    render() {
+        return (
+            <div className="displayNote" style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}  >
+            {this.props.noteData.map(o => (
+
+                <Card className='singleNoteDisplay'>
+                    <PinDropIcon style={{ 'margin-left': '200px' }} />
+                    <div>
+                        {o.title}
+                    </div>
+                    <div>
+                        {o.content}
+                    </div>
+                    <div>
+                        {/* <NoteIconOpration></NoteIconOpration> */}
+                    </div>
+                   < NoteIconOpration data = {o.noteId}></NoteIconOpration>                      
+                </Card>
+            ))
+       }
+        </div>
+        );
+    }
+}
+
+export default DisplayUnpinNotes;
