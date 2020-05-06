ac870c904c274a911a50fe61e06ca6b4cb43997e
varad <varad2011>
2020-05-06 22:21:09 +0530

    added trash icon class file

---
+++ fundooapp/src/components/TrashIcons.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TrashIcons extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default TrashIcons;
