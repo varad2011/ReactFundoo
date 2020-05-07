4cfc9bd114e1a5fd16175c3785b24dab264137bc
varad <varad2011>
2020-05-07 22:51:00 +0530

    added delete and untrash icons with css

--- fundooapp/src/components/TrashIcons.jsx
+++ fundooapp/src/components/TrashIcons.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import RestoreFromTrashIcon from '@material-ui/icons/RestoreFromTrash';
 class TrashIcons extends Component {
     render() {
         return (
             <div>
-                
+                <div className="iconNotes">
+                <TrashIcons></TrashIcons>
+                </div>
+                <div className="iconNotes">
+                <RestoreFromTrashIcon></RestoreFromTrashIcon>
+                </div>        
             </div>
         );
     }
