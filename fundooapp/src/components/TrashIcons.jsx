f79b9861fe3a531062dc723eccca23f93cb80bc3
varad <varad2011>
2020-05-09 00:21:51 +0530

    added delete and untrash methods

--- fundooapp/src/components/TrashIcons.jsx
+++ fundooapp/src/components/TrashIcons.jsx
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import RestoreFromTrashIcon from '@material-ui/icons/RestoreFromTrash';
+import {unTrash} from '../components/Service';
+import {deleteNote} from '../components/Service';
 class TrashIcons extends Component {
+
+    unTrashNote= (noteId) =>{
+        unTrash(noteId).then(Response => {
+            console.log(Response.data.message)
+          })
+            .catch((error) => {
+              alert(error.response.message)
+            })
+     }
+     deleteNotes = (noteId) =>{
+        deleteNote(noteId).then(Response => {
+            console.log(Response.data.message)
+          })
+            .catch((error) => {
+              alert(error.response.message)
+            })
+     }
+
     render() {
+        const { noteId } = this.props;
         return (
-            <div>
+            <div style={{
+                "padding-bottom": '10px',
+                "padding-top": '5px', "display": 'flex',
+                "flex-direction": 'row'
+            }}>
                 <div className="iconNotes">
-                <TrashIcons></TrashIcons>
+                    <DeleteForeverIcon onClick ={()=>{ this.deleteNotes(noteId);}} ></DeleteForeverIcon>
                 </div>
                 <div className="iconNotes">
-                <RestoreFromTrashIcon></RestoreFromTrashIcon>
-                </div>        
+                    <RestoreFromTrashIcon onClick ={()=>{ this.unTrashNote(noteId);}} ></RestoreFromTrashIcon>
+                </div>
             </div>
         );
     }
 }
+
 
 export default TrashIcons;
