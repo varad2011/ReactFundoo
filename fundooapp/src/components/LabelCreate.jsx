03f18102dbb7ad2d62ec1ac113009426377dbfb8
varad <varad2011>
2020-05-09 20:42:03 +0530

    added label create,update,delete methods

--- fundooapp/src/components/LabelCreate.jsx
+++ fundooapp/src/components/LabelCreate.jsx
@@ -12,51 +12,127 @@ import Avatar from '@material-ui/core/Avatar';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import { TextField } from '@material-ui/core';
+import DoneIcon from '@material-ui/icons/Done';
+import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined';
+import DeleteIcon from '@material-ui/icons/Delete';
+import {newLabelCreate} from '../components/Service';
+import{labelUpdate}  from '../components/Service';
+import {deleteLabel} from '../components/Service';
+
 class LabelCreate extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
-            open : false,
-            handleClose:false
+        this.state = {
+            open: false,
+            handleClose: false,
+            labelName: ''
         };
     }
-    openDilogbox =()=>{
-this.setState({
-    open:true
-})
+    openDilogbox = () => {
+        this.setState({
+            open: true
+        })
     }
     handleClose = () => {
         this.setState({ open: false });
-      };
-    
+    };
+
     // handleClose = ()=>{
     //     this.setState({
     //         open:false
     //     })
     // }
+    handlChange = (event) => {
+        console.log("handle change")
+        this.setState({
+            [event.target.name]: event.target.value
+        }, () =>
+            console.log(this.state, '---->name'))
+
+    }
+    createLable = () => {
+        let labelModel={};
+        labelModel.labelName = this.state.labelName;
+        newLabelCreate(labelModel).then(Response => {
+            alert(Response.data.message)
+            console.log(Response.data)
+        })
+            .catch((error) => {
+                console.log(this.state.store)
+                alert(error.response.message)
+            })
+    };
+    updateLabel = (labelId) => {
+        let labelModel={};
+        labelModel.labelName = this.state.labelName;
+        labelModel.labelId = labelId;
+        labelUpdate(labelModel).then(Response => {
+            alert(Response.data.message)
+            console.log(Response.data)
+        })
+            .catch((error) => {
+                console.log(this.state.store)
+                alert(error.response.message)
+            })
+    }
+    LabelDelete = (labelId) => {
+        let labelModel={};
+        labelModel.labelId = labelId;
+        deleteLabel(labelModel).then(Response => {
+            alert(Response.data.message)
+            console.log(Response.data)
+        })
+            .catch((error) => {
+                console.log(this.state.store)
+                alert(error.response.message)
+            })
+    }
+
+
     render() {
         const { onClose } = this.props;
         return (
             <div>
                 <MenuItem onClick={() => this.openDilogbox()}>
-                 <ListItemIcon><EditOutlinedIcon /></ListItemIcon>
-                 <ListItemText  onClick = {()=>{ this.openDilogbox()}}>Create Label</ListItemText>
-               </MenuItem>
-               <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.state.open} >
-                   <DialogTitle>Create Label</DialogTitle>
-                   <List>
-                   {this.props.listOfLabel.map((o) => (
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar >
-                <LabelOutlinedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={o.labelName} />
-          </ListItem>
-        ))}
-                   </List>
-               </Dialog>
+                    <ListItemIcon><EditOutlinedIcon /></ListItemIcon>
+                    <ListItemText onClick={() => { this.openDilogbox() }}>Create Label</ListItemText>
+                </MenuItem>
+                <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.state.open} >
+                    <DialogTitle>Create Label</DialogTitle>
+
+                    <List>
+
+                        <ListItem>
+                            <ListItemAvatar>
+
+                                <ClearOutlinedIcon />
+
+                            </ListItemAvatar>
+                            <TextField placeholder="create New label" name = "labelName"
+                             onChange={this.handlChange}
+                                ></TextField>
+                            <ListItemAvatar>
+
+                                <DoneIcon  onClick ={()=>{this.createLable();}}/>
+
+                            </ListItemAvatar>
+                        </ListItem>
+                        {this.props.listOfLabel.map((o) => (
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <DeleteIcon onClick ={() =>{this.LabelDelete(o.labelId)}}></DeleteIcon>
+                                    {/* <LabelOutlinedIcon /> */}
+                                </ListItemAvatar>
+                                <TextField placeholder={o.labelName} name = "labelName"  onChange={this.handlChange}> </TextField>
+                                {/* <ListItemText primary={o.labelName} /> */}
+                                <ListItemAvatar>
+                                    <EditOutlinedIcon onClick = {()=>{this.updateLabel(o.labelId)}} />
+                                </ListItemAvatar>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Dialog>
             </div>
         );
     }
