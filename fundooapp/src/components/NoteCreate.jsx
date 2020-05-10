6e1cc68c44012af2d82240d6e6d285092d82fa23
varad <varad2011>
2020-05-11 01:52:57 +0530

    adjust divs and close open div method

--- fundooapp/src/components/NoteCreate.jsx
+++ fundooapp/src/components/NoteCreate.jsx
@@ -4,6 +4,8 @@ import { pinUnpin } from '../components/Service';
 import NoteIconOpration from '../components/NoteIconOpration';
 import PinDropIcon from '@material-ui/icons/PinDrop';
 import { createNote } from '../components/Service'
+import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
 class NoteCreate extends Component {
     constructor(props) {
         super()
@@ -13,7 +15,11 @@ class NoteCreate extends Component {
             noteOpen: false
         }
     }
-
+    closeCreateNote = () => {
+        this.setState({
+            noteOpen: false
+        });
+    }
     changeNoteOpenToFalse = () => {
         this.setState({
             noteOpen: true
@@ -43,7 +49,6 @@ class NoteCreate extends Component {
             alert(Response.data.message);
             console.log(Response.data.message)
         }).catch((error) => {
-
             console.log(error.response)
             alert(error.response)
         })
@@ -52,13 +57,11 @@ class NoteCreate extends Component {
     setPinToNote = (id) => {
         console.log(id)
         pinUnpin(id)
-            
             .then(Response => {
                 console.log(Response)
                 alert(Response.data.message)
             })
             .catch((error) => {
-
                 console.log(error.response)
                 alert(error.response.data)
             })
@@ -67,48 +70,58 @@ class NoteCreate extends Component {
         //    const noteOpenToCreate = ({this.openFlag})=>(
         //    <div>{openFlag?}</div>
         //    );
-
-
-
-
         return (
             <div>
-                {/*                 
-            //     <div >
-            //      <TextField placeholder = "noteName" name ="title" onChange = {this.handlChange} ></TextField>
-            // </div> */}
-
-
                 <div className="createNote" >
-
-                    <Container>
-
+                    <Container >
                         <div>
-                            <TextField placeholder="noteName" name="title" onChange={this.handlChange} onClick={this.changeNoteOpenToFalse}></TextField>
-                            {this.state.noteOpen ? <PinDropIcon className="pin"  /> :null}
-                        </div>
+                            {this.state.noteOpen ?
+                                <div>
+                                    <div>
+                                        <TextField placeholder="title" name="title" onChange={this.handlChange} onClick={this.changeNoteOpenToFalse} style={{ "margin-top": '10px' }}></TextField>
+                                        <div className="pinNotesInCreate" style={{ 'backgroundImage': 'url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICA8cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+CiAgPHBhdGggZmlsbD0iIzAwMCIgZD0iTTE3IDR2N2wyIDN2MmgtNnY1bC0xIDEtMS0xdi01SDV2LTJsMi0zVjRjMC0xLjEuOS0yIDItMmg2YzEuMTEgMCAyIC44OSAyIDJ6TTkgNHY3Ljc1TDcuNSAxNGg5TDE1IDExLjc1VjRIOXoiLz4KPC9zdmc+Cg==)' }} />
+                                    </div>
 
+                                    <div>
+                                        <TextField placeholder="Description" name="content" onChange={this.handlChange} style={{ "margin-top": '10px' }}
+                                        ></TextField>
+                                        <NoteIconOpration />
+                                    </div>
+                                    <div className="createNoteButton">
+                                        <Button onClick={this.newNotecreate}>saveNote</Button>
+                                        <Button onClick={this.closeCreateNote}>close</Button>
+                                    </div>
+                                </div>
+                                :
+                                <InputBase placeholder="take Note" onChange={this.handlChange} onClick={this.changeNoteOpenToFalse} style={{ "margin-top": '10px' }}></InputBase>
+                            }
+                        </div>
+                    </Container>
+
+                </div>
+                {/* <div className="createNote" >
+                    <Container >
+                        <div>
+                            <TextField placeholder="take Note" name="title" onChange={this.handlChange} onClick={this.changeNoteOpenToFalse} style={{ "margin-top": '10px' }}></TextField>
+                            {this.state.noteOpen ? <div className="pinNotesInCreate" style={{ 'backgroundImage': 'url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICA8cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+CiAgPHBhdGggZmlsbD0iIzAwMCIgZD0iTTE3IDR2N2wyIDN2MmgtNnY1bC0xIDEtMS0xdi01SDV2LTJsMi0zVjRjMC0xLjEuOS0yIDItMmg2YzEuMTEgMCAyIC44OSAyIDJ6TTkgNHY3Ljc1TDcuNSAxNGg5TDE1IDExLjc1VjRIOXoiLz4KPC9zdmc+Cg==)' }} /> : null}
+                        </div>
                         {this.state.noteOpen ?
                             <div>
-                                <TextField placeholder="Description" name="content" onChange={this.handlChange}
+                                <TextField placeholder="Description" name="content" onChange={this.handlChange} style={{ "margin-top": '10px' }}
                                 ></TextField>
                                 <NoteIconOpration />
-
                             </div> : null}
                         {this.state.noteOpen ?
-                            <div>
-                                <button onClick={this.newNotecreate}>saveNote</button>
+                            <div className = "createNoteButton">
+                                <Button onClick={this.newNotecreate}>saveNote</Button>
+                                <Button>close</Button>
                             </div> : null}
                     </Container>
                     <div>
-
                     </div>
-
-                </div>
+                </div> */}
             </div>
-
         );
     }
 }
-
 export default NoteCreate;
