240d1900b07bd123894e2b30e6e2d864b213470d
varad <varad2011>
2020-05-13 20:04:24 +0530

    added get set profile pic methods

--- fundooapp/src/components/ProfilePicSet.jsx
+++ fundooapp/src/components/ProfilePicSet.jsx
@@ -5,43 +5,80 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Avatar from '@material-ui/core/Avatar';
 import Input from '@material-ui/core/Input';
 import { AppBar, Button, Toolbar, IconButton, ListItemIcon } from '@material-ui/core';
+import { setprofilePic } from '../components/Service';
+import { updateLoginUserProfilePic } from '../components/Service';
 
 class ProfilePicSet extends Component {
-    state = {
-        anchorEl: null,
-    }
-    handleClick = event => {
-        this.setState({ anchorEl: event.currentTarget });
-    }
+  state = {
+    anchorEl: null,
+    file: null,
+    profilePic: ''
+  }
 
-    handleClose = () => {
-        this.setState({ anchorEl: null });
-    }
+  componentDidMount() {
+    this.setprofileUpload();
+  }
 
-    render() {
-        const { anchorEl} = this.state;
-        return (
-            <div>
-                <Avatar  
-                 onClick={this.handleClick}
-                type="file" alt="Remy Sharp" src="/static/images/avatar/1.jpg" ></Avatar>
-                <Menu
-                  // style = {{ "width":'140px'}}
-                  id="iconNotes"
-                  anchorEl={anchorEl}
-                  open={Boolean(anchorEl)}
-                  onClose={this.handleClose}
-                >
-                    <MenuItem>                 
-                  <Input type="file" /> 
-                  </MenuItem>   
-                  <MenuItem>             
-                  <Button>save</Button>                  
-                    </MenuItem>                 
-                </Menu>
-            </div>
-        );
-    }
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  }
+  onFileChange = event => {
+    this.setState({ file: event.target.files[0] });
+    console.log("file", this.state.selectedFile)
+  }
+
+  saveImage = () => {
+    const formData = new FormData();
+    formData.append('file', this.state.file);
+    setprofilePic(formData).then(Response => {
+      this.setprofileUpload();
+      this.handleClose();
+    })
+      .catch((error) => {
+        alert(error.response.message)
+      })
+  }
+
+  setprofileUpload = () => {
+    updateLoginUserProfilePic().then(Response => {
+      this.setState({
+        profilePic: Response.data.data
+      })
+    })
+      .catch((error) => {
+        alert(error.response.message)
+      })
+  }
+
+  render() {
+    const { anchorEl } = this.state;
+    return (
+      <div>
+        <Avatar
+          onClick={this.handleClick}
+          alt="Remy Sharp"
+          src={this.state.profilePic} >
+        </Avatar>
+        <Menu
+          // style = {{ "width":'140px'}}
+          id="iconNotes"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose} >
+          <MenuItem>
+            <Input type="file" name="file" onChange={this.onFileChange} />
+          </MenuItem>
+          <MenuItem>
+            <Button onClick={this.saveImage}>save</Button>
+          </MenuItem>
+        </Menu>
+      </div>
+    );
+  }
 }
 
 export default ProfilePicSet;
